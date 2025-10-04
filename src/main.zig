b0b3b7b237497c57842c282d8059c9185fecfdc0
Willem van der Schyff <willemvds@gmail.com>
2025-10-04 15:02:19 +0200

    feat: Provide user feedback for WriteLengthMismatch.

--- src/main.zig
+++ src/main.zig
@@ -6,8 +6,20 @@ var ally = gpa.allocator();
 
 pub fn main() !void {
     const installed_path = install_please.imgpls(ally) catch |err| {
+        switch (err) {
+            error.AccessDenied => {
+                std.debug.print("\x1B[38;5;196mAccess denied\x1B[0m. Check permissions or use 'sudo' to run as root.\n", .{});
+                return;
+            },
+            error.WriteLengthMismatch => {
+                std.debug.print("\x1B[38;5;196mWrite Length Mismatch\x1B[0m. Possible that written binary is corrupted. Please try running install again.\n", .{});
+                return;
+            },
+            else => {
+                return err;
+            },
+        }
         if (err == error.AccessDenied) {
-            std.debug.print("\x1B[38;5;196mAccess denied\x1B[0m. Check permissions or use 'sudo' to run as root.\n", .{});
             return;
         }
 
