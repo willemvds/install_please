90139628e8ae021f86d02d2ab6c5a41ce0f18ee4
Willem van der Schyff <willemvds@gmail.com>
2025-12-20 13:59:17 +0200

    feat: Some basic icon support.

--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,9 @@
 const std = @import("std");
 
+const desktop_config = @embedFile("imgpls.desktop");
 const imgpls_binary = @embedFile("imgpls");
-const desktop_config = @embedFile("ImagePlease.desktop");
+const scalable_icon = @embedFile("pls.svg");
+const icon = @embedFile("128.png");
 
 const ExitCode = enum(u8) {
     Ok = 0,
@@ -12,15 +14,33 @@ const ExitCode = enum(u8) {
 pub fn imgpls(a: std.mem.Allocator) ![]const u8 {
     const install_path = "/usr/bin/imgpls";
     const bin_fh = try std.fs.createFileAbsolute(install_path, .{ .truncate = true, .mode = 0o755 });
+    defer bin_fh.close();
     const bin_written = try bin_fh.write(imgpls_binary);
     if (bin_written != imgpls_binary.len) {
         return error.WriteLengthMismatch;
     }
 
-    const desktop_path = "/usr/share/applications/ImagePlease.desktop";
+    const desktop_path = "/usr/share/applications/imgpls.desktop";
     const desktop_fh = try std.fs.createFileAbsolute(desktop_path, .{ .truncate = true, .mode = 0o644 });
+    defer desktop_fh.close();
     const desktop_written = try desktop_fh.write(desktop_config);
     if (desktop_written != desktop_config.len) {
+        return error.WriteLengthMismatch;
+    }
+
+    const scalable_icon_path = "/usr/share/icons/hicolor/scalable/apps/imgpls.svg";
+    const scalable_icon_fh = try std.fs.createFileAbsolute(scalable_icon_path, .{ .truncate = true, .mode = 0o644 });
+    defer scalable_icon_fh.close();
+    const scalable_icon_written = try scalable_icon_fh.write(scalable_icon);
+    if (scalable_icon_written != scalable_icon.len) {
+        return error.WriteLengthMismatch;
+    }
+
+    const icon_path = "/usr/share/icons/hicolor/128x128/apps/imgpls.svg";
+    const icon_fh = try std.fs.createFileAbsolute(icon_path, .{ .truncate = true, .mode = 0o644 });
+    defer icon_fh.close();
+    const icon_written = try icon_fh.write(icon);
+    if (icon_written != icon.len) {
         return error.WriteLengthMismatch;
     }
 
@@ -37,7 +57,7 @@ pub fn imgpls(a: std.mem.Allocator) ![]const u8 {
         user,
         "xdg-mime",
         "default",
-        "ImagePlease.desktop",
+        "imgpls.desktop",
         "image/png",
     }, a);
 
